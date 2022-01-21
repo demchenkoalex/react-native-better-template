852d2bf5cb9c03bae73fa4bc53ea133ee717eec4
Alex Demchenko <alexdemchenko@yahoo.com>
2022-01-21 21:05:06 +0100

    Fix file extension

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -67,7 +67,7 @@ const App = () => {
           }}
         >
           <Section title='Step One'>
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title='See Your Changes'>
